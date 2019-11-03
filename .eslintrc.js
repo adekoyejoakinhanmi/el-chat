160f5c0f1446de03e056a4a11427fa1698861a85
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2019-11-03 21:12:21 +0100

    off

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   ],
   rules: {
     // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
